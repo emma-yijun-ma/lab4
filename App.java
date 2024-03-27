6c585ac360382cd2e6bc21dbc41350f4e12f72a1
taciturn <151200155+emma-yijun-ma@users.noreply.github.com>
2024-03-26 21:17:20 -0700

    java version

---
+++ App.java
@@ -0,0 +1,17 @@
+
+
+public class App {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+
+        root.left = right;
+        root.right = left;
+
+        return root;
+    }
+}
